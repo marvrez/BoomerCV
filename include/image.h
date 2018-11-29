793700e23a3ef7047dca3c907d71a8136a1332e7
Marvin Reza <marvinreza@outlook.com>
2018-11-29 13:15:36 +0100

    :camera: add image processing api :camera:

---
+++ include/image.h
@@ -0,0 +1,74 @@
+#ifndef IMAGE_H
+#define IMAGE_H
+
+typedef struct {
+    int c, h, w;
+    float* data;
+} image;
+
+typedef struct {
+    int x, y;
+    int w, h;  
+    float score;
+    const char name; 
+} box;
+
+typedef struct {
+    float x, y;
+} point;
+
+image make_image(int w, int h, int c);
+image make_empty_image(int w, int h, int c);
+image make_random_image(int w, int h, int c);
+image copy_image(image m);
+void free_image(image m);
+
+// load/save functions
+image load_image(const char* filename);
+image load_image_memory(const unsigned char* buffer, int buf_len, int c);
+int save_image_png(image m, const char* path);
+int save_image_jpg(image m, const char* path, int quality);
+
+image get_channel(image m, int c);
+
+// colorspace functions
+void rgb_to_hsv(image m);
+void hsv_to_rgb(image m);
+void rgb_to_bgr(image m);
+void bgr_to_rgb(image m);
+void yuv_to_rgb(image m);
+void rgb_to_yuv(image m);
+image rgb_to_grayscale(image m);
+image grayscale_to_rgb(image m, float r, float g, float b);
+
+// image operations
+void translate_image(image m, float s);
+void scale_image(image m, float s);
+void normalize_image(image m);
+void transpose_image(image m);
+void flip_image(image m);
+
+image rotate_image(image m, float rad);
+image crop_image(image m, int dx, int dy, int w, int h);
+image rotate_crop_image(image m, float rad, float s, int w, int h, float dx, float dy, float aspect);
+
+// resizing
+float nn_interpolate(image m, float x, float y, int c);
+image nn_resize(image m, int w, int h);
+float bilinear_interpolate(image m, float x, float y, int c);
+image bilinear_resize(image m, int w, int h);
+
+// thresholding
+image threshold_image(image m, float thresh);
+image otsu_binarize_image(image m);
+image binarize_image(image m, int reverse);
+image dilate_image(image m, int times);
+image erode_image(image m, int times);
+
+image gaussian_noise_reduce(image grayscale);
+image equalize_histogram(image m);
+
+unsigned char* get_image_data_hwc(image m);
+void free_image_data(unsigned char* data);
+
+#endif
