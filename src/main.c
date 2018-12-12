02e1243a26ec10cf1dc9242a2bf37e5bfc002a2c
Marvin Reza <marvinreza@outlook.com>
2018-12-12 01:20:52 +0100

    able to run binarize from main

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 
 extern void run_resize(int argc, char** argv);
 extern void run_grayscale(int argc, char** argv);
+extern void run_binarize(int argc, char** argv);
 
 int main(int argc, char** argv)
 {
@@ -17,6 +18,9 @@ int main(int argc, char** argv)
     }
     else if (strcmp(argv[1], "grayscale") == 0) {
         run_grayscale(argc, argv);
+    }
+    else if (strcmp(argv[1], "binarize") == 0) {
+        run_binarize(argc, argv);
     }
     else {
         fprintf(stderr, "%s is not a valid option\n", argv[1]);
